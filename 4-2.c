5fedfa790192987e6f2613f1cad4daaacc20423a
Jiaguli <69131640+Jiaguli@users.noreply.github.com>
2020-11-20 23:17:02 +0800

    Add files via upload

---
+++ 4-2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int input[10];
+int a=0;
+while(a!=4){
+    print("1.PUSH\n 2.LIST\n 3.QUIT\n 4.FINISH\n");
+    printf("請輸入你想要用的功能\n");
+    scanf("%d",a);
+    if(a==1){
+
+
+    else(a==2){
+
+
+
+    else(a==3){
+
+
+
+    else(a==4){
+        break
+    }
+    }
+    }
+    }
+}
+}
+//老師，我找不到L4的補課，而且作業也太難了吧www
